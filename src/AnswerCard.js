ab9c65f881caa2602d7fe79c14439859aa194e6a
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-01-07 20:34:26 -0700

    adds localStorage persistance

--- src/AnswerCard.js
+++ src/AnswerCard.js
@@ -7,7 +7,7 @@ function AnswerCard(props) {
       <h3 className="answer-card--question">{props.questionInfo.question}</h3>
       <h3 className="answer-card--correct-answer">Correct: {props.questionInfo.correctAnswer}</h3>
       <a href={props.questionInfo.link} className="answer-card--link" target="_blank" rel="noopener noreferrer">More Info</a>
-      <button onClick={props.saveCard}>Store Answer For Later</button>
+      <button onClick={props.putInLocalStorage}>Store Answer For Later</button>
     </div>
   )
 }
